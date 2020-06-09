12af5a8f1a441cfbab50d012a7f6e2f306240f92
stevescruz <steve.silverio.cruz@gmail.com>
2020-06-09 15:55:14 -0400

    Styled Components & Global Style II (Dashboard)
    -react-icons library ADDED
    
    -Assets folder CREATED
      -Included github-background.svg
      -Included logo.svg
    
    -Global Styles EDITED
      -Included global styles for body, input, button, and #root.
    
    -Dashboard index EDITED
      -Included <img> with logo.
      -Included <form> with an <input> and <button> to seach for a repository.
      -Included <div> that contains an <a> to the desired repository.
      -Included a <img> with the repository picture in the <a>.
      -Included a <div> containing <strong> with the repository title and <p> with
    the repository description in <a>.
      -Included an chevron right icon in <a>
    
    -Dashboard styles EDITED
      -Included styling for the individual parts of the Dashboard.
      -The styling includes a background-image for the Dashboard.
      -The styling includes a hover transitions for the <button> that takes 0.2 seconds
    to darken the color shade.
      -The styling includes a hover transition for the <a> that takes 0.2 seconds to
    move it to the right.
      -The styling includes a hover transition for the chevron right icon that takes
    0.2 seconds to darken the color shade.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,111 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-	return <Title>Explore repositories at GitHub.</Title>;
+	return (
+		<>
+			<img src={logoImg} alt="Explore GitHub" />
+			<Title>Explore repositories at GitHub.</Title>
+			<Form>
+				<input placeholder="Type the repository name" />
+				<button type="submit">Search</button>
+			</Form>
+			<Repositories>
+				<a href="test">
+					<img
+						src="https://avatars3.githubusercontent.com/u/60204789?s=460&u=dfa937c94e27d06e251e8dbc076c94110d4641aa&v=4"
+						alt="Steve Cruz"
+					/>
+					<div>
+						<strong>stevescruz/explore_github</strong>
+						<p>Find exciting projects.</p>
+					</div>
+					<FiChevronRight
+						size={20}
+						title="Click this arrow to view the repository"
+					/>
+				</a>
+
+				<a href="test">
+					<img
+						src="https://avatars3.githubusercontent.com/u/60204789?s=460&u=dfa937c94e27d06e251e8dbc076c94110d4641aa&v=4"
+						alt="Steve Cruz"
+					/>
+					<div>
+						<strong>stevescruz/explore_github</strong>
+						<p>Find exciting projects.</p>
+					</div>
+					<FiChevronRight
+						size={20}
+						title="Click this arrow to view the repository"
+					/>
+				</a>
+
+				<a href="test">
+					<img
+						src="https://avatars3.githubusercontent.com/u/60204789?s=460&u=dfa937c94e27d06e251e8dbc076c94110d4641aa&v=4"
+						alt="Steve Cruz"
+					/>
+					<div>
+						<strong>stevescruz/explore_github</strong>
+						<p>Find exciting projects.</p>
+					</div>
+					<FiChevronRight
+						size={20}
+						title="Click this arrow to view the repository"
+					/>
+				</a>
+
+				<a href="test">
+					<img
+						src="https://avatars3.githubusercontent.com/u/60204789?s=460&u=dfa937c94e27d06e251e8dbc076c94110d4641aa&v=4"
+						alt="Steve Cruz"
+					/>
+					<div>
+						<strong>stevescruz/explore_github</strong>
+						<p>Find exciting projects.</p>
+					</div>
+					<FiChevronRight
+						size={20}
+						title="Click this arrow to view the repository"
+					/>
+				</a>
+
+				<a href="test">
+					<img
+						src="https://avatars3.githubusercontent.com/u/60204789?s=460&u=dfa937c94e27d06e251e8dbc076c94110d4641aa&v=4"
+						alt="Steve Cruz"
+					/>
+					<div>
+						<strong>stevescruz/explore_github</strong>
+						<p>Find exciting projects.</p>
+					</div>
+					<FiChevronRight
+						size={20}
+						title="Click this arrow to view the repository"
+					/>
+				</a>
+
+				<a href="test">
+					<img
+						src="https://avatars3.githubusercontent.com/u/60204789?s=460&u=dfa937c94e27d06e251e8dbc076c94110d4641aa&v=4"
+						alt="Steve Cruz"
+					/>
+					<div>
+						<strong>stevescruz/explore_github</strong>
+						<p>Find exciting projects.</p>
+					</div>
+					<FiChevronRight
+						size={20}
+						title="Click this arrow to view the repository"
+					/>
+				</a>
+			</Repositories>
+		</>
+	);
 };
 
 export default Dashboard;
