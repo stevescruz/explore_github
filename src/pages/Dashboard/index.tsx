f53c1ca463e134db5f79d392992b0fd1517aec71
stevescruz <steve.silverio.cruz@gmail.com>
2020-06-13 04:58:45 -0400

    Connecting to the GitHub API with Axios
    -Axios ADDED
    
    -services directory CREATED
    -API.ts file CREATED
      -Added connection with the GitHub API using Axios.
    
    -Index.tsx from Dashboard EDITED
      -Created handleAddRepository function to use Axios Get HTTP Method
    to find a given GitHub repository and add it's data to the application.
      -Renders <a> components by using map on the repositories.
    
    -Styles.ts from Dashboard EDITED
      -Added optimization to the <div> nested in <a> to properly fit the image
    inside <a> and avoid the text from very long description getting to close
    to the icon on the right margin of <a>.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,108 +1,70 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
-import { Title, Form, Repositories } from './styles';
 import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
+
+interface Repository {
+	full_name: string;
+	description: string;
+	owner: {
+		login: string;
+		avatar_url: string;
+	};
+	// stargazers_count: number;
+	// forks_count: number;
+	// open_issues_count: string;
+}
 
 const Dashboard: React.FC = () => {
+	const [newRepository, setNewRepository] = useState('');
+	const [repositories, setRepositories] = useState<Repository[]>([]);
+
+	async function handleAddRepository(
+		event: FormEvent<HTMLFormElement>,
+	): Promise<void> {
+		event.preventDefault();
+
+		const res = await api.get<Repository>(`/repos/${newRepository}`);
+
+		const repository = res.data;
+
+		setRepositories([...repositories, repository]);
+		setNewRepository('');
+	}
+
 	return (
 		<>
 			<img src={logoImg} alt="Explore GitHub" />
 			<Title>Explore repositories at GitHub.</Title>
-			<Form>
-				<input placeholder="Type the repository name" />
+			<Form onSubmit={handleAddRepository}>
+				<input
+					value={newRepository}
+					onChange={e => setNewRepository(e.target.value)}
+					placeholder="Type the repository name"
+				/>
 				<button type="submit">Search</button>
 			</Form>
 			<Repositories>
-				<a href="test">
-					<img
-						src="https://avatars3.githubusercontent.com/u/60204789?s=460&u=dfa937c94e27d06e251e8dbc076c94110d4641aa&v=4"
-						alt="Steve Cruz"
-					/>
-					<div>
-						<strong>stevescruz/explore_github</strong>
-						<p>Find exciting projects.</p>
-					</div>
-					<FiChevronRight
-						size={20}
-						title="Click this arrow to view the repository"
-					/>
-				</a>
-
-				<a href="test">
-					<img
-						src="https://avatars3.githubusercontent.com/u/60204789?s=460&u=dfa937c94e27d06e251e8dbc076c94110d4641aa&v=4"
-						alt="Steve Cruz"
-					/>
-					<div>
-						<strong>stevescruz/explore_github</strong>
-						<p>Find exciting projects.</p>
-					</div>
-					<FiChevronRight
-						size={20}
-						title="Click this arrow to view the repository"
-					/>
-				</a>
-
-				<a href="test">
-					<img
-						src="https://avatars3.githubusercontent.com/u/60204789?s=460&u=dfa937c94e27d06e251e8dbc076c94110d4641aa&v=4"
-						alt="Steve Cruz"
-					/>
-					<div>
-						<strong>stevescruz/explore_github</strong>
-						<p>Find exciting projects.</p>
-					</div>
-					<FiChevronRight
-						size={20}
-						title="Click this arrow to view the repository"
-					/>
-				</a>
-
-				<a href="test">
-					<img
-						src="https://avatars3.githubusercontent.com/u/60204789?s=460&u=dfa937c94e27d06e251e8dbc076c94110d4641aa&v=4"
-						alt="Steve Cruz"
-					/>
-					<div>
-						<strong>stevescruz/explore_github</strong>
-						<p>Find exciting projects.</p>
-					</div>
-					<FiChevronRight
-						size={20}
-						title="Click this arrow to view the repository"
-					/>
-				</a>
-
-				<a href="test">
-					<img
-						src="https://avatars3.githubusercontent.com/u/60204789?s=460&u=dfa937c94e27d06e251e8dbc076c94110d4641aa&v=4"
-						alt="Steve Cruz"
-					/>
-					<div>
-						<strong>stevescruz/explore_github</strong>
-						<p>Find exciting projects.</p>
-					</div>
-					<FiChevronRight
-						size={20}
-						title="Click this arrow to view the repository"
-					/>
-				</a>
-
-				<a href="test">
-					<img
-						src="https://avatars3.githubusercontent.com/u/60204789?s=460&u=dfa937c94e27d06e251e8dbc076c94110d4641aa&v=4"
-						alt="Steve Cruz"
-					/>
-					<div>
-						<strong>stevescruz/explore_github</strong>
-						<p>Find exciting projects.</p>
-					</div>
-					<FiChevronRight
-						size={20}
-						title="Click this arrow to view the repository"
-					/>
-				</a>
+				{repositories.map(repository => {
+					return (
+						<a key={repository.full_name} href="test">
+							<img
+								src={repository.owner.avatar_url}
+								alt={repository.owner.login}
+							/>
+							<div>
+								<strong>{repository.full_name}</strong>
+								<p>{repository.description}</p>
+							</div>
+							<FiChevronRight
+								size={20}
+								title="Click this arrow to view the repository"
+							/>
+						</a>
+					);
+				})}
 			</Repositories>
 		</>
 	);
